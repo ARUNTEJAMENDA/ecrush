7b3f17bebbdb09217706793ad666184d3d4ac795
aruntejamenda <aruntejamenda@gmail.com>
2022-08-12 22:38:00 +0530

    night work

--- original/ecrush.sql
+++ original/ecrush.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 12, 2022 at 01:57 PM
+-- Generation Time: Aug 12, 2022 at 07:07 PM
 -- Server version: 10.4.22-MariaDB
 -- PHP Version: 8.1.1
 
